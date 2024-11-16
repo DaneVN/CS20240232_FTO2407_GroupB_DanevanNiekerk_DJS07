e117eda72e71dcdd36160bee4f06c19c77313899
DaneVN <Dane.vN23@gmail.com>
2024-11-16 08:04:46 +0200

    React Components Header and Meme-Form added and styled

---
+++ components/Meme.jsx
@@ -0,0 +1,36 @@
+//eslint-disable-next-line
+import React from "react";
+
+function Meme() {
+  return (
+    <main>
+      <form className="form">
+        <div>
+          <label htmlFor="top-text" className="form--label">
+            Top Text
+          </label>
+          <input
+            id="top-text"
+            type="text"
+            placeholder="Shut up"
+            className="form--input"
+          />
+        </div>
+        <div>
+          <label htmlFor="bottom-text" className="form--label">
+            Bottom Text
+          </label>
+          <input
+            id="bottom-text"
+            type="text"
+            placeholder="and take my money"
+            className="form--input"
+          />
+        </div>
+        <button className="form--button">Get a new meme image 🖼</button>
+      </form>
+    </main>
+  );
+}
+
+export default Meme;
